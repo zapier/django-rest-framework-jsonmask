c5cae3e25c4ff48c5ce2faf183c88b448b8e82c2
Craig Labenz <craig.labenz@gmail.com>
2018-11-29 11:34:29 -0500

    Bumping rc

--- rest_framework_jsonmask/__init__.py
+++ rest_framework_jsonmask/__init__.py
@@ -3,6 +3,6 @@ from __future__ import unicode_literals
 """Package for DRF-jsonmask"""
 
 __project__ = "restframework_jsonmask"
-__version__ = "0.1.0rc1"
+__version__ = "0.1.0rc2"
 
 VERSION = __project__ + '-' + __version__
